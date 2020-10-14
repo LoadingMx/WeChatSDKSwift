c04788ed178ca1cdc3423ae33505a33c32611ac9
LiuSha <liusha1666@gmail.com>
2020-10-14 12:13:53 +0800

    Initial Commit

---
+++ WeChatSDKSwift/WeChatSDKSwift.h
@@ -0,0 +1,18 @@
+//
+//  WeChatSDKSwift.h
+//  WeChatSDKSwift
+//
+//  Created by liusha on 2020/10/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeChatSDKSwift.
+FOUNDATION_EXPORT double WeChatSDKSwiftVersionNumber;
+
+//! Project version string for WeChatSDKSwift.
+FOUNDATION_EXPORT const unsigned char WeChatSDKSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeChatSDKSwift/PublicHeader.h>
+
+
